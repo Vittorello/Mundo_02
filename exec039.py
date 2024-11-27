577db49adab6010e47dfa5e8844d905382ed2ae6
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-27 01:30:44 -0300

    Acresentando a validação de sexo

--- exec039.py
+++ exec039.py
@@ -3,21 +3,25 @@ from datetime import datetime
 while True:
     try:
         nome = str(input("Digite o seu nome: "))
+        sexo = str(input("Digite o seu sexo: ")).lower()
         ano_nascimento = int(input("Digite o seu ano de nascimento: "))
         idade = datetime.now().year - ano_nascimento
         idade_faltante = 18 - idade
         idade_sobrando = idade - 18
 
-        if idade == 18:
-            print(f"Parabéns {nome}, está na hora de você se alistar!")
+        if sexo == 'feminino':
+            print("Mulheres não precisam se alistar.")
             break
-        elif idade < 18:
-            print(f"Olá {nome}, você ainda não possui idade suficiente para se alistar, espere mais {idade_faltante} anos para se alistar.")
-            break
-        elif idade > 18:
-            print(f"Olá {nome}, já passou {idade_sobrando} anos do seu tempo de alistamento.")
-            break
-
+        elif sexo == 'masculino':
+            if idade == 18:
+                print(f"Parabéns {nome}, está na hora de você se alistar!")
+                break
+            elif idade < 18:
+                print(f"Olá {nome}, você ainda não possui idade suficiente para se alistar, espere mais {idade_faltante} anos para se alistar.")
+                break
+            elif idade > 18:
+                print(f"Olá {nome}, já passou {idade_sobrando} anos do seu tempo de alistamento.")
+                break
     except ValueError:
         print("Digite os dados corretamente.")
 
